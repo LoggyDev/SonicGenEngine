b9ef9a1922fa7851c7b0567d94cacf59a3f29ac8
Loggy Developer <60048714+LoggyDev@users.noreply.github.com>
2021-05-30 16:18:40 -0500

    Add files via upload

---
+++ include/engine.hpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <SDL2/sdl.h>
+#include <math.h>
+#include "window.hpp"
+
+class engine {
+public:
+	void engine_limit_fps ();
+	void engine_init ();	
+	void engine_run (); 
+	void engine_process_events ();
+
+	window          window;
+	SDL_Rect        rect;
+	SDL_Rect        rect1;
+    SDL_Event       event;
+	bool            running=1;
+
+protected:
+	const int       fps=60;
+	const int       frameDelay=1000/fps;
+	uint32_t        frameStart;
+	int             frameTime;
+};
---
+++ include/window.hpp
@@ -0,0 +1,6 @@
+class window {
+public:
+	void window_init ();
+	SDL_Window* window;
+	SDL_Renderer* renderer;
+};
