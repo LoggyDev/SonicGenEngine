8c73946cd245be36d295fec00380d2df3d12c90b
Loggy Developer <60048714+LoggyDev@users.noreply.github.com>
2021-05-30 22:23:20 -0500

    added some stuffs
    -comments

--- include/engine.hpp
+++ include/engine.hpp
@@ -2,6 +2,7 @@
 #include <SDL2/sdl.h>
 #include <math.h>
 #include "window.hpp"
+#include "object.hpp"
 
 class engine {
 public:
@@ -9,14 +10,16 @@ public:
 	void engine_init ();	
 	void engine_run (); 
 	void engine_process_events ();
+	void engine_draw (); 
+	void engine_update ();
 
-	window          window;
-	SDL_Rect        rect;
-	SDL_Rect        rect1;
-    	SDL_Event       event;
-	bool            running=1;
+	window          window;    // create an object of the window class...
+	object          obj_test;  // create an test object...
+    SDL_Event       event;     // create an event for input etc...
+	bool            running=1; // bool for game loop...
 
-protected:
+	// variables for constant framerate...
+
 	const int       fps=60;
 	const int       frameDelay=1000/fps;
 	uint32_t        frameStart;
